699ecb7eb63085fe60ccd64e5eb2bea762ccdee4
Fill <gcodepost@gmail.com>
2019-07-12 03:32:24 +0300

    Build_02(twig)

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var { watch, src, dest, parallel, series } = require('gulp');
 var browserSync = require('browser-sync');
 var del = require('del');
+var twig = require('gulp-twig');
+var imagemin = require('gulp-imagemin');
 
 // Девсервер
 function devServer(cb) {
@@ -14,15 +16,34 @@ function devServer(cb) {
   browserSync.create().init(params);
   cb();
 }
+
+
+  
+  function buildAssets(cb) {
+    // Уберём пока картинки из общего потока
+    src(['src/assets/**/*.*', '!src/assets/img/**/*.*'])
+      .pipe(dest('build/assets/'));
+  
+    src('src/assets/img/**/*.*')
+      .pipe(imagemin())
+      .pipe(dest('build/assets/img'));
+  
+    // Раньше функция что-то вовзращала, теперь добавляем вместо этого искусственый колбэк
+    // Это нужно, чтобы Галп понимал, когда функция отработала и мог запустить следующие задачи
+    cb();
+  }
+  
 function clearBuild() {
   return del('build/');
 }
 
 // Сборка
 function buildPages() {
-  return src('src/*.html')
-    .pipe(dest('build/'));
-}
+    // Пути можно передавать массивами
+    return src(['src/pages/*.twig', 'src/pages/*.html'])
+      .pipe(twig())
+      .pipe(dest('build/'));
+  }
 
 function buildStyles() {
   return src('src/styles/*.css')
@@ -39,13 +60,13 @@ function buildAssets() {
     .pipe(dest('build/assets/'));
 }
 
-// Отслеживание
+
 function watchFiles() {
-  watch('src/*.html', buildPages);
-  watch('src/styles/*.css', buildStyles);
-  watch('src/scripts/**/*.js', buildScripts);
-  watch('src/assets/**/*.*', buildAssets);
-}
+    watch(['src/pages/*.twig', 'src/pages/*.html'], buildPages);
+    watch('src/styles/*.css', buildStyles);
+    watch('src/scripts/**/*.js', buildScripts);
+    watch('src/assets/**/*.*', buildAssets);
+  }
 
 exports.default =
   series(
